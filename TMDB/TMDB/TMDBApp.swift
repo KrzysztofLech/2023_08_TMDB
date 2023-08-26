1b29a68f81812abf0ab5b9cb938a9c23c6f701e4
Krzysztof Lech <>
2023-08-26 08:57:17 +0200

    Xcode init project

---
+++ TMDB/TMDB/TMDBApp.swift
@@ -0,0 +1,13 @@
+//  TMDBApp.swift
+//  Created by Krzysztof Lech on 26/08/2023.
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
